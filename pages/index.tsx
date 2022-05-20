89bae04aaeeb72d2d21b69e2c8301744251a68f7
Rodrigo Noal <rodrigonoal@gmail.com>
2022-05-20 14:30:21 -0300

    testing image import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
 const Home: NextPage = () => {
+  
+
   return (
     <div className={styles.container}>
 
       <div className='text-3x-l font-bold underline'>
-        Hello World
+        <Image src='/logo.svg' alt="logo" width="24" height="24"/> Hello World
       </div>
      
     </div>
